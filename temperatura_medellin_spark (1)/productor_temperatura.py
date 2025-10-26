3a942363ad3f70ed5b20fef1ccd20c379dab0792
RojasOr24 <lindirojas@gmail.com>
2025-10-26 07:48:34 -0500

    Update productor_temperatura.py

--- temperatura_medellin_spark (1)/productor_temperatura.py
+++ temperatura_medellin_spark (1)/productor_temperatura.py
@@ -1,16 +1,17 @@
+#se importan las librerias necesarias
 from kafka import KafkaProducer
 import pandas as pd
 import json, time
-
+#lee los datos historicos
 df = pd.read_csv("historical-weather-medellin.csv")
-
+#indica donde se encuentra el kafka
 producer = KafkaProducer(
     bootstrap_servers=['localhost:9092'],
     value_serializer=lambda v: json.dumps(v).encode('utf-8')
 )
 
 print(" Enviando datos a Kafka topic: clima_medellin")
-
+#envia los datos topic
 for _, row in df.iterrows():
     mensaje = {
         "date": str(row["date"]),

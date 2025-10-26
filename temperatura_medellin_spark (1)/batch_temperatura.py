4c7bf668b3942fd65375b2a58c5fb7045a787890
RojasOr24 <lindirojas@gmail.com>
2025-10-26 07:40:46 -0500

    Update batch_temperatura.py

--- temperatura_medellin_spark (1)/batch_temperatura.py
+++ temperatura_medellin_spark (1)/batch_temperatura.py
@@ -1,18 +1,20 @@
+# se crean Spark , para la ejecucion de DataFrame y SQL en PYsPARK.
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, avg, max, min
 
 spark = SparkSession.builder.appName("TemperaturaMedellinBatch").getOrCreate()
+#se carga los datos 
 data_path = "historical-weather-medellin.csv"
 df = spark.read.csv(data_path, header=True, inferSchema=True)
-
+#muestra muestra la estructura y se verifica que los datos se leen correctamente
 print("  Esquema de datos:")
 df.printSchema()
 
 print("  Muestra de datos:")
 df.show(5)
-
+#se limpian los datos y se eliminan los valores nulos
 df_clean = df.select("date", "tempmax", "tempmin", "temp").dropna()
-
+#se calcula los valores agregados en el conjunto de datos
 stats = df_clean.agg(
     avg("tempmax").alias("promedio_max"),
     avg("tempmin").alias("promedio_min"),
@@ -22,7 +24,7 @@ stats = df_clean.agg(
 )
 
 print("  Estadísticas generales de temperatura:")
-stats.show()
-stats.write.mode("overwrite").csv("resultados_temperatura_batch/")
-print("  Resultados guardados en carpeta: resultados_temperatura_batch/")
+stats.show() #muestra las estadisticas calculadas
+stats.write.mode("overwrite").csv("resultados_temperatura_batch/") #guarda los resultados en la carpeta
+print("  Resultados guardados en carpeta: resultados_temperatura_batch/") #detalle el resultado
 
